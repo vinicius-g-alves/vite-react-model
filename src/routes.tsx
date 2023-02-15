1758507c2a5d6f3ba0b8b8d2797dfe78da6c0931
vinicius-g-alves <viniss201@gmail.com>
2023-02-15 17:08:14 -0300

    add router file

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,67 +1,16 @@
-const Routes = ({ t }: { t: TFunction }): ReactElement<unknown> => {
+import { ReactElement, Suspense } from "react";
+import { Route, Routes as Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+
+const Routes = (): ReactElement<unknown> => {
+  // <Suspense fallback={<LoadingPage />}></Suspense>
   return (
-    <Router>
-      <Suspense fallback={<LoadingPage />}>
-        <Switch>
-          <Route
-            exact
-            path="/home"
-            render={(props) => (
-              <RenderComponent
-                menuKey="home"
-                useMainLayout
-                tabTitle={t("tab.home")}
-              >
-                <Home {...props} />
-              </RenderComponent>
-            )}
-          />
-
-          <Route
-            exact
-            path="/graphs"
-            render={(props) => (
-              <RenderComponent
-                menuKey="graphs"
-                useMainLayout
-                tabTitle={t("tab.graphs")}
-              >
-                <Graphs {...props} />
-              </RenderComponent>
-            )}
-          />
-
-          <Route
-            exact
-            path="/icons"
-            render={(props) => (
-              <RenderComponent
-                menuKey="icons"
-                useMainLayout
-                tabTitle={t("tab.icons")}
-              >
-                <Icons {...props} />
-              </RenderComponent>
-            )}
-          />
-
-          <Route
-            path={["/", "/auth"]}
-            render={() => (
-              <RenderComponent
-                authRoute
-                publicRoute
-                absoluteTabTitle
-                tabTitle={t("tab.authentication")}
-              >
-                <Auth />
-              </RenderComponent>
-            )}
-          />
-        </Switch>
-      </Suspense>
-    </Router>
+    <Suspense>
+      <Switch>
+        <Route path="/home" element={<HomePage />} />
+      </Switch>
+    </Suspense>
   );
 };
 
-export default withTranslation()(Routes);
+export default Routes;
