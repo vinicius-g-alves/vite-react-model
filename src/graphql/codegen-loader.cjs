5381c1c4c3143af0f27c7d5c01b235e19592dfce
Matheus Camilotto <mckcamilotto@gmail.com>
2023-03-08 17:06:01 -0300

    build: add graphql code generation

---
+++ src/graphql/codegen-loader.cjs
@@ -0,0 +1,25 @@
+require("dotenv").config({ path: ".env.local" });
+const fetch = require("cross-fetch");
+const { getIntrospectionQuery, buildClientSchema } = require("graphql");
+
+const API_URL = process.env.VITE_API_URL;
+
+module.exports = async () => {
+  const introspectionQuery = getIntrospectionQuery();
+
+  if (!API_URL) {
+    throw new Error("environment variable `API_URL` is not defined");
+  }
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query: introspectionQuery }),
+  });
+
+  const data = await response.json();
+
+  return buildClientSchema(data.data);
+};
