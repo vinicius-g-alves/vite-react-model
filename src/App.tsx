b7232371a75ea17f5b7e3c6a89b17c6b3a210062
Matheus Camilotto <mckcamilotto@gmail.com>
2023-03-08 11:41:18 -0300

    feat: add urql client to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import { ReactElement } from "react";
 import { ConfigProvider } from "antd";
 
 import Routes from "./routes";
+import { createClient, Provider } from "urql";
+
+const client = createClient({
+  url: import.meta.env.VITE_API_URL,
+});
 
 function App(): ReactElement<unknown> {
   return (
-    <ConfigProvider>
-      <Routes />
-    </ConfigProvider>
+    <Provider value={client}>
+      <ConfigProvider>
+        <Routes />
+      </ConfigProvider>
+    </Provider>
   );
 }
 
